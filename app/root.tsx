cf38a3d93815d3178090f5b242b726889adb7b55
dip-hayato-yokoyama <hayato-yokoyama@dip-net.co.jp>
2025-05-29 16:28:25 +0900

    ルートコンポーネントに非同期ローダーを追加し、連絡先データを取得して表示する機能を実装。連絡先がない場合のメッセージも追加。

--- app/root.tsx
+++ app/root.tsx
@@ -9,10 +9,17 @@ import {
 import type { Route } from "./+types/root";
 
 import appStylesHref from "./app.css?url";
+import { getContacts } from "./data";
 
-// めも: root.tsx は、「/」に対応するコンポーネントです。
+// MEMO: root.tsx は、「/」に対応するコンポーネント → RootRoute
 
-export default function App() {
+export async function clientLoader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
+
+export default function App({ loaderData }: Route.ComponentProps) { // MEMO: Route.ComponentProps
+  const { contacts } = loaderData;
   return (
     <>
       <div id="sidebar">
@@ -33,14 +40,30 @@ export default function App() {
           </Form>
         </div>
         <nav>
-          <ul>
-            <li>
-              <Link to={`/contacts/1`}>Your Name</Link>
-            </li>
-            <li>
-              <Link to={`/contacts/2`}>Your Friend</Link>
-            </li>
-          </ul>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}
+                    {contact.favorite ? (
+                      <span>★</span>
+                    ) : null}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
         </nav>
       </div>
       <div id="detail">

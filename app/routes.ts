3120d0503ac7e1b58e7ab0fbc5d587510d5363ec
dip-hayato-yokoyama <hayato-yokoyama@dip-net.co.jp>
2025-05-29 17:57:02 +0900

    連絡先編集用の新しいルートを追加し、編集フォームを実装。連絡先の取得と更新処理を非同期で行うアクション関数を追加。

--- app/routes.ts
+++ app/routes.ts
@@ -9,6 +9,7 @@ export default [
   layout("layouts/sidebar.tsx", [
     index("routes/home.tsx"), // MEMO: 「/」に対応するルートを指定
     route("contacts/:contactId", "routes/contact.tsx"),
+    route("contacts/:contactId/edit", "routes/edit-contact.tsx"),
   ]),
   route("about", "routes/about.tsx"),
 ] satisfies RouteConfig;
